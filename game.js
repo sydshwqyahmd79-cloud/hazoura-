5f49b1b25a5cb17a0ea630dcc6201000fe1c97a2
sydshwqyahmd79-cloud <sydshwqyahmd79@gmail.com>
2026-09-01 18:02:39 +0300

    Update game.js

--- game.js
+++ game.js
@@ -8,7 +8,6 @@ let arrows = [];
 let monsters = [];
 let score = 0;
 
-// الصور بالاسماء اللي انت رافعها
 const background = new Image();
 background.src = 'background.jpg.jpeg'; 
 
@@ -21,33 +20,30 @@ monsterImg.src = 'monster.png.jpeg';
 const arrowImg = new Image();
 arrowImg.src = 'arrow.png'; 
 
-// اللاعب
 const archer = { x: 50, y: 450, w: 60, h: 80 };
 
-// الوحوش
 function spawnMonster() {
     monsters.push({ x: 800, y: 450, w: 60, h: 60, speed: 2 });
 }
 setInterval(spawnMonster, 2000);
 
-// التحكم
-document.addEventListener('keydown', e => { keys[e.key] = true; });
+document.addEventListener('keydown', e => { 
+    keys[e.key] = true; 
+    if(e.key === ' ') arrows.push({ x: archer.x + 50, y: archer.y + 30, w: 30, h: 10, speed: 8 });
+});
 document.addEventListener('keyup', e => { keys[e.key] = false; });
 
-function shoot() {
-    arrows.push({ x: archer.x + 50, y: archer.y + 30, w: 30, h: 10, speed: 8 });
-}
-
-// تحديث اللعبة
 function update() {
     if (keys['ArrowRight']) archer.x += 5;
     if (keys['ArrowLeft']) archer.x -= 5;
-    if (keys[' ']) { shoot(); keys[' '] = false; }
+    if(archer.x < 0) archer.x = 0;
+    if(archer.x > 740) archer.x = 740;
 
     arrows.forEach(a => a.x += a.speed);
     monsters.forEach(m => m.x -= m.speed);
+    arrows = arrows.filter(a => a.x < 800);
+    monsters = monsters.filter(m => m.x > -60);
 
-    // التصادم
     arrows.forEach((a, i) => {
         monsters.forEach((m, j) => {
             if (a.x < m.x + m.w && a.x + a.w > m.x && a.y < m.y + m.h && a.y + a.h > m.y) {
@@ -59,21 +55,6 @@ function update() {
     });
 }
 
-// الرسم
 function draw() {
     ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
-    ctx.drawImage(archerImg, archer.x, archer.y, archer.w, archer.h);
-    arrows.forEach(a => ctx.drawImage(arrowImg, a.x, a.y, a.w, a.h));
-    monsters.forEach(m => ctx.drawImage(monsterImg, m.x, m.y, m.w, m.h));
-    ctx.fillStyle = 'white';
-    ctx.font = '24px Arial';
-    ctx.fillText('Score: ' + score, 20, 40);
-}
-
-// اللوب
-function gameLoop() {
-    update();
-    draw();
-    requestAnimationFrame(gameLoop);
-}
-gameLoop();
+    ctx
