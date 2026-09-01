ab0970fa6067bd612a7b76a294d42358191fcd68
sydshwqyahmd79-cloud <sydshwqyahmd79@gmail.com>
2026-09-01 17:09:59 +0300

    Update game.js

--- game.js
+++ game.js
@@ -4,7 +4,13 @@ canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
 let score = 0; let lives = 3; let arrows = []; let monsters = [];
-let archerX = canvas.width / 2; let archerY = canvas.height - 80;
+let archerX = canvas.width / 2; let archerY = canvas.height - 120;
+
+// حمل الصور
+const archerImg = new Image(); archerImg.src = 'archer.png';
+const monsterImg = new Image(); monsterImg.src = 'monster.png';
+const arrowImg = new Image(); arrowImg.src = 'arrow.png';
+const bgImg = new Image(); bgImg.src = 'background.jpg';
 
 function updateUI() {
   document.getElementById('score').innerText = score;
@@ -14,21 +20,39 @@ function updateUI() {
 // لمسه واحده = حركه + ضرب
 canvas.addEventListener('touchstart', (e) => {
   archerX = e.touches[0].clientX;
-  arrows.push({x: archerX, y: archerY, speed: 8});
-canvas.addEventListener('touchmove', (e) => {
-  archerX = e.touches[0].clientX;
-  arrows.push({x: archerX, y: archerY, speed: 8});
+  arrows.push({x: archerX, y: archerY, speed: 10});
 });
 
-// الوحوش
-setInterval(() => { monsters.push({ x: Math.random() * canvas.width, y: 0, speed: 2 }); }, 2000);
+canvas.addEventListener('touchmove', (e) => {
+  archerX = e.touches[0].clientX;
+  arrows.push({x: archerX, y: archerY, speed: 10});
+});
+
+setInterval(() => {
+  monsters.push({ x: Math.random() * canvas.width, y: 0, speed: 2.5 });
+}, 1500);
 
 function gameLoop() {
-  ctx.clearRect(0,0,canvas.width,canvas.height);
-  ctx.fillStyle = '#8B4513'; ctx.fillRect(canvas.width/2 - 50, canvas.height - 60, 100, 60);
-  ctx.fillStyle = 'blue'; ctx.beginPath(); ctx.arc(archerX, archerY, 15, 0, Math.PI*2); ctx.fill();
-  arrows.forEach((a, i) => { a.y -= a.speed; ctx.fillStyle = 'black'; ctx.fillRect(a.x, a.y, 3, 15); if(a.y < 0) arrows.splice(i,1); });
-  monsters.forEach((m, i) => { m.y += m.speed; ctx.fillStyle = 'green'; ctx.beginPath(); ctx.arc(m.x, m.y, 20, 0, Math.PI*2); ctx.fill(); });
+  // ارسم الخلفية HD
+  ctx.drawImage(bgImg, 0, 0, canvas.width, canvas.height);
+
+  // ارسم الرامي
+  ctx.drawImage(archerImg, archerX - 25, archerY - 25, 50, 50);
+
+  // الاسهم
+  arrows.forEach((a, i) => {
+    a.y -= a.speed;
+    ctx.drawImage(arrowImg, a.x - 5, a.y, 10, 30);
+    if(a.y < 0) arrows.splice(i,1);
+  });
+
+  // الوحوش
+  monsters.forEach((m, i) => {
+    m.y += m.speed;
+    ctx.drawImage(monsterImg, m.x - 25, m.y, 50, 50);
+  });
+
   requestAnimationFrame(gameLoop);
 }
-updateUI(); gameLoop();
+
+bgImg.onload = () => { updateUI(); gameLoop(); }
