b24b1a4e3b69816b49c7638573170c2b59f81a59
sydshwqyahmd79-cloud <sydshwqyahmd79@gmail.com>
2026-09-01 17:06:43 +0300

    Update game.js

--- game.js
+++ game.js
@@ -3,104 +3,32 @@ const ctx = canvas.getContext('2d');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
-let score = 0;
-let lives = 3;
-let arrows = [];
-let monsters = [];
-let archerX = canvas.width / 2;
-let archerY = canvas.height - 80;
-let lastShot = 0; // عشان نعمل فاصل بين الاسهم
+let score = 0; let lives = 3; let arrows = []; let monsters = [];
+let archerX = canvas.width / 2; let archerY = canvas.height - 80;
 
 function updateUI() {
   document.getElementById('score').innerText = score;
   document.getElementById('lives').innerText = '❤️'.repeat(lives);
 }
 
-function shootArrow() {
-  let now = Date.now();
-  if(now - lastShot > 200) { // يضرب كل 0.2 ثانيه بس عشان ميبقاش سريع اوي
-    arrows.push({x: archerX, y: archerY, speed: 8});
-    lastShot = now;
-  }
-}
-
 // لمسه واحده = حركه + ضرب
 canvas.addEventListener('touchstart', (e) => {
   archerX = e.touches[0].clientX;
-  shootArrow();
-});
-
+  arrows.push({x: archerX, y: archerY, speed: 8});
 canvas.addEventListener('touchmove', (e) => {
   archerX = e.touches[0].clientX;
-  shootArrow(); // يضرب وهو بيتحرك
+  arrows.push({x: archerX, y: archerY, speed: 8});
 });
 
-// ينزل وحش كل ثانيتين
-setInterval(() => {
-  monsters.push({
-    x: Math.random() * canvas.width,
-    y: 0,
-    speed: 2
-  });
-}, 2000);
-
-function checkCollision(a, m) {
-  let dx = a.x - m.x;
-  let dy = a.y - m.y;
-  return Math.sqrt(dx*dx + dy*dy) < 25;
-}
+// الوحوش
+setInterval(() => { monsters.push({ x: Math.random() * canvas.width, y: 0, speed: 2 }); }, 2000);
 
 function gameLoop() {
   ctx.clearRect(0,0,canvas.width,canvas.height);
-
-  // ارسم القلعة
-  ctx.fillStyle = '#8B4513';
-  ctx.fillRect(canvas.width/2 - 50, canvas.height - 60, 100, 60);
-
-  // ارسم الرامي
-  ctx.fillStyle = 'blue';
-  ctx.beginPath();
-  ctx.arc(archerX, archerY, 15, 0, Math.PI*2);
-  ctx.fill();
-
-  // الاسهم
-  arrows.forEach((a, i) => {
-    a.y -= a.speed;
-    ctx.fillStyle = 'black';
-    ctx.fillRect(a.x, a.y, 3, 15);
-    if(a.y < 0) arrows.splice(i,1);
-  });
-
-  // الوحوش
-  monsters.forEach((m, i) => {
-    m.y += m.speed;
-    ctx.fillStyle = 'green';
-    ctx.beginPath();
-    ctx.arc(m.x, m.y, 20, 0, Math.PI*2);
-    ctx.fill();
-
-    arrows.forEach((a, j) => {
-      if(checkCollision(a, m)) {
-        score += 10;
-        monsters.splice(i,1);
-        arrows.splice(j,1);
-        updateUI();
-      }
-    });
-
-    if(m.y > canvas.height - 60) {
-      lives--;
-      monsters.splice(i,1);
-      updateUI();
-    }
-  });
-
-  if(lives <= 0) {
-    alert('انتهت اللعبة! نقاطك: ' + score);
-    return;
-  }
+  ctx.fillStyle = '#8B4513'; ctx.fillRect(canvas.width/2 - 50, canvas.height - 60, 100, 60);
+  ctx.fillStyle = 'blue'; ctx.beginPath(); ctx.arc(archerX, archerY, 15, 0, Math.PI*2); ctx.fill();
+  arrows.forEach((a, i) => { a.y -= a.speed; ctx.fillStyle = 'black'; ctx.fillRect(a.x, a.y, 3, 15); if(a.y < 0) arrows.splice(i,1); });
+  monsters.forEach((m, i) => { m.y += m.speed; ctx.fillStyle = 'green'; ctx.beginPath(); ctx.arc(m.x, m.y, 20, 0, Math.PI*2); ctx.fill(); });
   requestAnimationFrame(gameLoop);
 }
-
-updateUI();
-gameLoop();
+updateUI(); gameLoop();
